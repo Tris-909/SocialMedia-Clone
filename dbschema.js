6728d292672f3329467361557443e020222524fb
= <tranminhtri9090@gmail.com>
2020-07-01 15:48:29 +1000

    Adding authentication register

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    posts: [
+        {
+            userHandle: 'user name',
+            body: 'body of the the post',
+            createdTime: 'time this post was posted',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,53 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
-admin.initializeApp();
-
 const express = require('express');
 const app = express();
 
+admin.initializeApp();
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD0tFUUX6Xut-RPSWDOD6ff9Yu6pDMa12Y",
+    authDomain: "socialapp-2c8b0.firebaseapp.com",
+    databaseURL: "https://socialapp-2c8b0.firebaseio.com",
+    projectId: "socialapp-2c8b0",
+    storageBucket: "socialapp-2c8b0.appspot.com",
+    messagingSenderId: "218503334984",
+    appId: "1:218503334984:web:3d3f40787b355c46be5c7d",
+    measurementId: "G-B5MJ5MKF51"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 app.get('/posts', (req, res) => {
     admin.firestore().collection('posts').get()
     .then(data => {
         let posts = [];
         data.forEach(doc => {
-            posts.push(doc.data());
+            posts.push({
+                postID: doc.id,
+                body: doc.data().body,
+                userHandle: doc.data().userHandle,
+                createdTime: doc.data().createdTime
+            });
         });
         return res.json(posts);
     })
     .catch(err => console.log(err));
 })
 
-exports.createPosts = functions.https.onRequest((request, response) => {
-    if (request.method !== 'POST') {
-        return res.status(400).json({error: "Method not allowed"});
-    }
-    
+app.post('/posts', (request, res) => {    
     const newPosts = {
         body: request.body.body,
         userHandle: request.body.userHandle,
-        createdTime: admin.firestore.Timestamp.fromDate(new Date())
+        createdTime: new Date().toISOString()
     };
     
-    admin.firestore()
+    db
         .collection('posts')
+        .orderBy('createdAt', 'desc')
         .add(newPosts)
         .then((doc) => {
             res.json({message: `document ${doc.id} created successfully`});
@@ -42,4 +58,51 @@ exports.createPosts = functions.https.onRequest((request, response) => {
         });
 });
 
-exports.api = functions.https.onRequest(app);
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle 
+    }
+    // VALIDATE DATA
+    let token, userId;
+
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if (doc.exists) {
+                return res.status(400).json({ handle: 'this handle is already taken'});
+            } else {
+                return firebase
+                .auth()
+                .createUserWithEmailAndPassword(newUser.email, newUser.password);
+            }
+        })
+        .then(data => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then(idToken => {
+            token = idToken;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdTime: new Date().toISOString(),
+                userID: userId
+            };
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() => {
+            return res.status(201).json({ token });
+        })
+        .catch(err => {
+            console.error(err);
+            if (err.code === "auth/email-already-in-use") {
+                return res.status(400).json({ email: 'Email is already registered'})
+            } else {
+                return res.status(500).json({ error: err.code });
+            }
+        })
+})
+
+exports.api = functions.region('asia-east2').https.onRequest(app);
