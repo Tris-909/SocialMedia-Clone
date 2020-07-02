57e47e8f9e285ede91875ab12a6963ae55fa26fc
= <tranminhtri9090@gmail.com>
2020-07-02 22:26:53 +1000

    Adding AddComment Route in the FirebaseFunc

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -45,3 +45,56 @@ exports.postOnePost = (req, res ) => {
             console.error(err);
         });
 }
+
+exports.getPost = (req, res) => {
+    let postData = {};
+    db.doc(`/posts/${req.params.postID}`)
+        .get()
+        .then(doc => {
+            if (!doc.exists){
+                return res.status(404).json({error: 'Post not found'})
+            }
+            postData = doc.data();
+            postData.postID = doc.id;
+            return db.collection('comments').where('postID', '==', req.params.postID).get();
+        })
+        .then(data => {
+            postData.comments = [];
+            data.forEach(doc => {
+                postData.comments.push(doc.data());
+            });
+            return res.json(postData);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: err.code});
+        });
+}
+
+exports.commentOnPost = (req, res) => {
+    if (req.body.body.trim() === '') {
+        return res.status(400).json({error: 'Command must not be empty'});
+    }
+
+    const newComment = {
+        body: req.body.body,
+        createdTime: new Date().toISOString(),
+        postID: req.params.postID,
+        userHandle: req.user.handle,
+        userImage: req.user.imageUrl
+    };
+
+    db.doc(`/posts/${req.params.postID}`).get()
+        .then(doc => {
+            if (!doc.exists) {
+                return res.status(404).json({error: 'This Post Is Not Exist'});
+            }
+            return db.collection('comments').add(newComment);
+        })
+        .then(() => {
+            res.json(newComment);
+        })
+        .catch(err => {
+            res.status(500).json({error: 'Something went wrong'});
+        });
+}
--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,19 @@ const app = express();
 
 const FBAuth = require('./utilities/FBAuth');
 
-const {getAllPosts, postOnePost} = require('./handlers/posts');
+const {getAllPosts, postOnePost, getPost, commentOnPost} = require('./handlers/posts');
 const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require('./handlers/users');
 
 // Posts route
 app.get('/posts', getAllPosts);
 app.post('/post', FBAuth, postOnePost);
+app.get('/post/:postID', getPost);
+app.post('/posts/:postID/comment', FBAuth, commentOnPost);
+// TODO : DELETE POST
+// TODO : LIKE A POST
+// TODO : UNLIKE A POST
+
+
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
