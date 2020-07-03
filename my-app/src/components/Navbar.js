a114cacde058f19e5b08dd77dfd757c465ec5ff4
= <tranminhtri9090@gmail.com>
2020-07-03 15:52:41 +1000

    Set Up the frontend Enviroment using: React, MaterialUI,...

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Link from 'react-router-dom/Link';
+// MUI stuff
+import Appbar from '@material-ui/core/Appbar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <Appbar position="fixed">
+                <Toolbar className="nav-container">
+                    <Button color="inherit" component={Link} to="/">Home</Button>
+                    <Button color="inherit" component={Link} to="/login">Login</Button>
+                    <Button color="inherit" component={Link} to="/signup">SignUp</Button>
+                </Toolbar>
+            </Appbar>
+        )
+    }
+}
+
+export default Navbar
---
+++ my-app/src/pages/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>HomePage</h1>
+            </div>
+        )
+    }
+}
+
+export default home
